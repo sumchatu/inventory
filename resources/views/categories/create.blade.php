4520ec3da3447f6aab880c57f7f32cb672f5c3ca
Sumanta <chattu.d@gmail.com>
2021-11-24 16:07:58 +0530

    all-code

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Categories</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item active">Category Create</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="card card-primary card-outline">
+              <div class="card-body">
+                <h5 class="card-title">Create New Category</h5> <br>
+            <!-- form start -->
+              <form action="{{route('categories.store')}}" method=post>
+                  @csrf
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Category Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter Category Name">
+                    @if($errors->has('name'))
+                      <span class="text-danger">{{$errors->first('name')}}</span>
+                    @endif
+                  </div>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-save"></i> Submit</button>
+                </div>
+              </form>
+              </div>
+            </div><!-- /.card -->
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+@endsection
